40ed22a8f77bdc6bd8808c9bec231a7c5422d235
JEWOO KWAK <65706899+jewookwak@users.noreply.github.com>
2024-05-21 19:30:24 +0900

    Update modified model.py

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
+
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 17 22:40:38 2021
+First created on Mon May 17 22:40:38 2021
 
-@author: Kiminjo
+@author: Kiminjo 
+
+Modified on Tue May 21 2024
+feat.Kwakjewoo
 """
 
 import torch
@@ -13,16 +17,6 @@ class CharRNN(nn.Module):
     def __init__(self, input_size, hidden_size=30, num_layer=1):
         super(CharRNN, self).__init__()
         
-        # Use torh.nn.RNN
-        # there is torch.nn.RNNCell but do not use this method
-        # Check the below document if you want to know difference between RNN and RNNCell in pytorch
-        # https://forum.onefourthlabs.com/t/difference-between-rnn-and-rnncell-in-pytorch/7643
-        # RNN support multiple stacked layer but RNNCell does not
-        # RNN needs several arguments
-        # 1. input size : in this case, size of unique character number 59
-        # 2. hidden size : hyperparameter, I set this 128
-        # 3. bias
-        # 4. num_layers
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layer = num_layer
@@ -42,17 +36,9 @@ class CharRNN(nn.Module):
     
     
     def init_hidden(self, batch_size):
-        # there are 4 weight parameters in RNN 
-        # input weight parameter W_x : size = (hidden_size, input_size)
-        # hidden weight parameter W_h : size = (hidden_size, hidden_size)
-        # bias input-hidden : size = (hidden)
-        # bias hidden-hidden : size=(hidden)
-        
-        hidden_state = Variable(torch.zeros(self.num_layer, batch_size, self.hidden_size))
+        hidden_state = torch.zeros(self.num_layer, batch_size, self.hidden_size)
         
         return hidden_state
-
-
 
 
 class CharLSTM(nn.Module):
@@ -78,14 +64,6 @@ class CharLSTM(nn.Module):
 
 
     def init_hidden(self, batch_size):
-        # there are 4 weight parameters in RNN 
-        # input weight parameter W_x : size = (hidden_size, input_size)
-        # hidden weight parameter W_h : size = (hidden_size, hidden_size)
-        # bias input-hidden : size = (hidden)
-        # bias hidden-hidden : size=(hidden)
-        
-        # LSTM needs hidden state and cell state 
-        
         weight = next(self.parameters()).data
         hidden_state = weight.new(self.num_layer, batch_size, self.hidden_size).zero_()
         cell_state = weight.new(self.num_layer, batch_size, self.hidden_size).zero_()
